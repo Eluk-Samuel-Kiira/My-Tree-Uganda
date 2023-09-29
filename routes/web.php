962682a8e4186a12fd6675b08d6841bb01d23375
kennethogrey <ogirekenneth@gmail.com>
2023-09-29 19:45:55 +0300

    fixed route error

--- routes/web.php
+++ routes/web.php
@@ -35,7 +35,7 @@ Route::get("/Government Agencies",[LandingPageController::class,'governmentAgenc
 Route::get("/Insurance Companies",[LandingPageController::class,'insuranceCompaniesPage'])->name('landing.insurance_companies');
 Route::get("/Media Companies",[LandingPageController::class,'mediaCompaniesPage'])->name('landing.media_companies');
 Route::post("/contact-form",[LandingPageController::class,'contactInfomation'])->name('contact.store');
-branRoute::post("/email-form",[LandingPageController::class,'emailInfomation'])->name('email.store');
+Route::post("/email-form",[LandingPageController::class,'emailInfomation'])->name('email.store');
 
 //landing page routes
 
