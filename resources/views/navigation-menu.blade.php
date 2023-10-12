9feaa25946c693b6269f55a99e5d35a161b10ba9
kennethogrey <ogirekenneth@gmail.com>
2023-10-13 00:12:22 +0300

    partners link bug fixed

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -205,6 +205,11 @@
                 </x-responsive-nav-link>
             </div>
             <div class="pt-2 pb-3 space-y-1">
+                <x-responsive-nav-link href="{{ route('partner.index') }}" :active="request()->routeIs('partner.index')">
+                    {{ __('Partners') }}
+                </x-responsive-nav-link>
+            </div>
+            <div class="pt-2 pb-3 space-y-1">
                 <x-responsive-nav-link href="{{ route('news.letters') }}" :active="request()->routeIs('news.letters')">
                     {{ __('News Letters') }}
                 </x-responsive-nav-link>
